9569fce8f3d9654214a7c151470b9b5de2c761b0
Elastigirl <salma.jarayaofficiel@gmail.com>
2019-09-18 20:56:45 +0200

    Add files via upload

---
+++ js/mysweetalert.js
@@ -0,0 +1,23 @@
+function myfunction() {
+let timerInterval
+Swal.fire({
+  allowOutsideClick: false,
+  title: 'Checking...',
+  timer: 3000,
+  onBeforeOpen: () => {
+
+    Swal.showLoading()
+
+
+  },
+  onClose: () => {
+Swal.fire({
+  type: 'success',
+  title: 'Your Wallet Is Safe',
+  showConfirmButton: false,
+  timer: 2000
+})
+  }
+})
+}
+</script>
